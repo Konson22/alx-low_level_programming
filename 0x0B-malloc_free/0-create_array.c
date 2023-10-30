f404f635c585014d3ead13c4d21ee43731005d21
Kon Akech <konakech3@gmail.com>
2023-10-30 16:44:20 -0700

    added new file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,8 +17,8 @@ unsigned int i;
 str = malloc(sizeof(char) * size);
 
 if (size == 0 || str == NULL)
-    return (NULL);
+return (NULL);
 for (i = 0; i < size; i++)
-    str[i] = c;
+str[i] = c;
 return (str);
 }
