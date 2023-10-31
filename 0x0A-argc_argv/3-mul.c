cb2acfbca75f69165c364a79734f8e20dee173e8
Kon Akech <konakech3@gmail.com>
2023-10-31 16:46:14 -0700

    add mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,17 +17,11 @@ f = 0;
 digit = 0;
 
 while (s[len] != '\0')
-{
 len++;
-}
 while (i < len && f == 0)
-{
 if (s[i] == '-')
-{
 ++d;
-}
 if (s[i] >= '0' && s[i] <= '9')
-{
 digit = s[i] - '0';
 if (d % 2)
 {
@@ -38,12 +32,8 @@ f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 f = 0;
-}
 i++;
-}
 if (f == 0)
-{
 return (0);
-}
 return (n);
 }
